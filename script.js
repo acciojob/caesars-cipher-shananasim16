48626ec98e0355e1199a7f89064dd6b7ef659bd4
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-12 23:40:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-function decodeROT13() {
-    const inputText = document.getElementById('input').value;
-    const decodedText = rot13(inputText);
-    document.getElementById('output').value = decodedText;
-}
+function  rot13(){
+            const inputText = document.getElementById("code").value
+            const decodedText = decodeROT13(inputText);
+            document.getElementById('result').innerText = '"'+decodedText'"';
+        }
 
-function rot13(encodedText) {
-    return encodedText.replace(/[A-Z]/g, (char) => {
-        return String.fromCharCode(((char.charCodeAt(0) - 65 + 13) % 26) + 65);
-    });
-}
-
-
- 
+        function decodeROT13(encodedText) {
+          return  encodedText.replace(/[A-Z]/g, (char) => {
+                return String.fromCharCode(((char.charCodeAt(0) - 65 + 13) % 26) + 65);
+            });
+        }
