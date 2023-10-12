e65fcd664e6b9e6914e0f2273010edcc33f61053
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-12 21:34:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,14 @@
-// Your Script here.
-
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
-
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return; //return decodedArr
+function decodeROT13() {
+    const inputText = document.getElementById('input').value;
+    const decodedText = rot13(inputText);
+    document.getElementById('output').value = decodedText;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+function rot13(encodedText) {
+    return encodedText.replace(/[A-Z]/g, (char) => {
+        return String.fromCharCode(((char.charCodeAt(0) - 65 + 13) % 26) + 65);
+    });
+}
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
 
-// Do not change this line
-window.rot13 = rot13;
+ 
