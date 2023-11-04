a2f7ca150afe9c4f4fa7a1395fac91c8211f902d
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-11-04 13:28:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,88 +1,57 @@
+// Your Script here.
+
 const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-  ".": ".",
-  ">": ">",
-  "<": "<",
-  "/": "/",
-  "\\": "\\",
-  "|": "|",
-  "-": "-",
-  : "",
-  "+": "+",
-  "=": "=",
-  "!": "!",
-  "@": "@",
-  "#": "#",
-  $: "$",
-  "%": "%",
-  "^": "^",
-  "&": "&",
-  "": "",
-  "(": "(",
-  ")": ")",
-  "{": "{",
-  "}": "}",
-  "[": "[",
-  "]": "]",
-  "\t": "\t",
-  "\n": "\n",
-  0: 0,
-  1: 1,
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5,
-  6: 6,
-  7: 7,
-  8: 8,
-  9: 9,
-  " ": " ",
-  '"': '"',
-  "'": "'",
-  ":": ":",
-  ";": ";",
-  "`": "`",
-};
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q",
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    "?": "?",
+    ",": ",",
+  };
+  
+  function rot13(encodedStr) {
+    let decodedArr = []; // Your Result goes here
+    // Only change code below this line
+    for(let i=0; i<encodedStr.length; i++){
+        const char=encodedStr[i];
+        if(lookup[char]!=undefined){
+            decodedArr.push(lookup[char]);
+        } else{
+            decodedArr.push(char);
+        }
+    }
+  
+    return decodedArr.join(""); //return decodedArr
+  }
+  
+  // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+  
+  // console.log(rot13("SERR YBIR? NPPVBWBO"));
+  
+  // Do not change this line
+  window.rot13 = rot13;
 
-function rot13(encodedStr) {
-  const mode = encodedStr ? 1 : 2;
-  encodedStr = encodedStr ?? document.getElementById("code").value;
-  const message = encodedStr
-    .toUpperCase()
-    .split("")
-    .map((char) => lookup[char])
-    .join("");
 
-  return mode === 1
-    ? message
-    : (document.getElementById("result").innerText = message);
-}
-
-window.rot13 = rot13;
+   
